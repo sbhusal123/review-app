bb9863347919c6d9afdd2cc8462e4f7e28b0b6e2
sbhusal123 <suryabhusal11@gmail.com>
2021-04-23 17:50:02 +0545

    Dynamic image loading.

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,18 +1,34 @@
 import React from "react";
 
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import Card from "../shared/card";
 
-import { globalStyles } from "../styles/global";
+import { globalStyles, images } from "../styles/global";
 
 export default function ReviewDetails({ navigation }) {
+    const rating = navigation.getParam("rating");
+
     return (
         <View style={globalStyles.container}>
             <Card>
                 <Text>{navigation.getParam("title")}</Text>
                 <Text>{navigation.getParam("body")}</Text>
-                <Text>{navigation.getParam("rating")}</Text>
+                <View style={style.rating}>
+                    <Text>Movie Rating: </Text>
+                    <Image source={images.ratings[rating]}></Image>
+                </View>
             </Card>
         </View>
     );
 }
+
+const style = StyleSheet.create({
+    rating: {
+        flexDirection: "row",
+        justifyContent: "center",
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: "#eee"
+    }
+});
