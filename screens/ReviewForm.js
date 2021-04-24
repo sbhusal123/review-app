784ed52da13f2be411296b17f4b0483cf5734251
sbhusal123 <suryabhusal11@gmail.com>
2021-04-24 13:30:08 +0545

    Input field validation schema with yup.

--- screens/ReviewForm.js
+++ screens/ReviewForm.js
@@ -1,9 +1,30 @@
 import React from "react";
 
-import { Button, TextInput, View } from "react-native";
+import { Button, TextInput, View, Text } from "react-native";
 import { Formik } from "formik";
 
 import { globalStyles } from "../styles/global";
+
+import * as yup from "yup";
+
+// Validation Schema
+const ReviewSchems = yup.object({
+    title: yup
+        .string()
+        .required()
+        .min(4),
+    body: yup
+        .string()
+        .required()
+        .min(8),
+    // Custom test based validation
+    rating: yup
+        .string()
+        .required()
+        .test("is-num-1-5", "Rating must be a number 1-5", val => {
+            return parseInt(val) < 6 && parseInt(val) > 0;
+        })
+});
 
 export default function ReviewForm({ addReview }) {
     return (
@@ -14,6 +35,7 @@ export default function ReviewForm({ addReview }) {
                     body: "",
                     rating: ""
                 }}
+                validationSchema={ReviewSchems}
                 onSubmit={(values, actions) => {
                     actions.resetForm();
                     addReview(values);
@@ -26,21 +48,40 @@ export default function ReviewForm({ addReview }) {
                             placeholder="Review Title"
                             onChangeText={formikProps.handleChange("title")}
                             value={formikProps.values.title}
+                            onBlur={formikProps.handleBlur("title")}
                         />
+                        <Text style={globalStyles.errorText}>
+                            {formikProps.touched.title &&
+                                formikProps.errors.title}
+                            {/*
+                                Not so consistent error 
+                                formikProps.errors.title
+                            */}
+                        </Text>
                         <TextInput
                             multiline
                             style={globalStyles.input}
                             placeholder="Review Body"
                             onChangeText={formikProps.handleChange("body")}
                             value={formikProps.values.body}
+                            onBlur={formikProps.handleBlur("body")}
                         />
+                        <Text style={globalStyles.errorText}>
+                            {formikProps.touched.body &&
+                                formikProps.errors.body}
+                        </Text>
                         <TextInput
                             style={globalStyles.input}
                             placeholder="Rating (1-5)"
                             onChangeText={formikProps.handleChange("rating")}
                             value={formikProps.values.rating}
                             keyboardType="numeric"
+                            onBlur={formikProps.handleBlur("rating")}
                         />
+                        <Text style={globalStyles.errorText}>
+                            {formikProps.touched.rating &&
+                                formikProps.errors.rating}
+                        </Text>
                         <Button
                             title="submit"
                             color="maroon"
